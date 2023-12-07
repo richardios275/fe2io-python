32cbec701d5e291f8331dcdd4b4c4321d1514b8a
Richelle Abigaile <richardios275@gmail.com>
2023-12-07 21:59:03 +0700

    Updated source
    This commit moves the audio handling from pydub to pygame. It also updates the UI to be more pixel perfect, and added an option to enable / disable fading. Bunch of bugs fixed, and added arguments. This commit also adds a requirements.txt which will be needed later if people want to use this thing

--- sources/audioPlayer.py
+++ sources/audioPlayer.py
@@ -1,33 +1,74 @@
 import requests
-from pydub import AudioSegment
-from pydub.playback import play
+import pygame
+from pygame import mixer_music
+from urllib.parse import urlparse
 
 volume = 70
+deathVolume = False
+fadein = True
 
-def change_volume(value):
+audio_cache = {}
+filename = ""
+
+def toggle_death_volume(value):
+    global deathVolume
+    deathVolume = value
+    set_volume(volume)
+
+def toggle_fadein(value):
+    global fadein
+    fadein = value
+
+def set_volume(vol):
     global volume
-    volume = value
+    volume = vol
+    if deathVolume == True:
+        vol = vol / 2
+    mixer_music.set_volume(vol / 100)
 
-async def download_and_play_audio(url):
-    print('bro')
-    try:
-        # Download the audio file
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for HTTP errors
+def get_file_extension(url):
+    # Parse the URL to get the path
+    path = urlparse(url).path
+    # Split the path based on '/'
+    parts = path.split('/')
+    # Get the last part of the path, which represents the file name
+    file_name = parts[-1]
+    # Split the file name based on '?' to handle parameters
+    file_name_parts = file_name.split('?')
+    # Get the first part of the file name, which represents the actual file name
+    actual_file_name = file_name_parts[0]
+    # Split the actual file name based on '.', and get the last part, which represents the file extension
+    extension = actual_file_name.split('.')[-1]
+    return extension
 
-        # Save the audio content to a temporary file
-        with open("temp_audio.mp3", "wb") as f:
-            f.write(response.content)
+def set_audio(url):
+    toggle_death_volume(False)
 
-        # Load the audio file using pydub
-        audio = AudioSegment.from_file("temp_audio.mp3")
+    # Load audio from cache if available, download if not.
+    if url in audio_cache:
+        filename = audio_cache[url]
+    else:
+        try:
+            if url == None:
+                url = 'https://github.com/anars/blank-audio/blob/master/250-milliseconds-of-silence.mp3'
+            # Download the audio file
+            response = requests.get(url)
+            response.raise_for_status()
 
-        # Volume adjustment
+            # Get the file extension of the audio
+            ext = get_file_extension(url)
+            filename = f"{len(audio_cache)}.{ext}"
+                
+            with open(filename, "wb") as f:
+                f.write(response.content)
 
-        
+            # Save audio to cache
+            audio_cache[url] = filename
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
 
-        # Play the audio
-        play(audio)
+    mixer_music.load(filename)
+    mixer_music.play(2, 0.00, 1000)
 
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+pygame.init()
+set_volume(volume)
--- sources/main.py
+++ sources/main.py
@@ -1,18 +1,16 @@
 import sys
+import argparse
 from PyQt6 import QtCore, QtGui, QtWidgets
 import asyncio
 import websockets
 import json
 from window import Ui_MainWindow
-from audioPlayer import download_and_play_audio
+from audioPlayer import set_audio, set_volume, toggle_death_volume, toggle_fadein
 
 # Variables
+args = None
 debounce = False
-volume = 70
 death_volume_reduction = True
-
-# Global UI
-global_ui = []
 
 # Handle Websocket
 async def connect_ws(username, status_label):
@@ -35,27 +33,30 @@ async def connect_ws(username, status_label):
                 data = json.loads(response)
                 if 'msgType' in data:
                     if data['msgType'] == 'bgm':
-                        asyncio.ensure_future(download_and_play_audio(data['audioUrl']))
-                    if data['msgType'] == 'gameStatus' and data['Status'] == 'died':
+                        set_audio(data['audioUrl'])
+                    if data['msgType'] == 'gameStatus' and data['statusType'] == 'died':
                         print('Player dead')
+                        if death_volume_reduction == True:
+                            toggle_death_volume(True)
             except json.JSONDecodeError:
                 pass  # Ignore non-JSON messages
 
 # Load the Main UI
 class MyMainWindow(QtWidgets.QDialog, Ui_MainWindow):
-    
-
-
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        
-        # Append UIs to the Global UI thing
-        global global_ui
-        global_ui.append(self.status_label)
 
-        # Connect the button's clicked signal to the custom function
+        # Set UI according to arguments
+        self.lineEdit.setText(args.username)
+        self.volume_label.setText(f"Volume: {args.volume}%")
+        self.volume_slider.setValue(int(args.volume))
+
+        # Connect the UI's to signals
         self.connect_button.clicked.connect(self.on_connect_button_clicked)
+        self.quieten_box.stateChanged.connect(self.on_quiten_box_state_changed)
+        self.fade_box.stateChanged.connect(self.on_fade_box_state_changed)
+        self.volume_slider.valueChanged.connect(self.on_slider_value_changed)
 
         # Timer to periodically run the event loop
         self.timer = QtCore.QTimer(self)
@@ -66,21 +67,47 @@ class MyMainWindow(QtWidgets.QDialog, Ui_MainWindow):
         # Run the asyncio event loop
         asyncio.get_event_loop().run_until_complete(asyncio.sleep(0))
 
+    # UI Functions
     def on_connect_button_clicked(self):
         global debounce
         if debounce == False:
             debounce = True
+            self.connect_button.setEnabled(False)
+            self.connect_button.setText("Connected")
             username = self.lineEdit.text()
-            print("Connecting")
             self.status_label.setText("Status: Connecting")
             asyncio.ensure_future(connect_ws(username, self.status_label))
 
+    def on_quiten_box_state_changed(value):
+        global death_volume_reduction
+        death_volume_reduction = False
+
+    def on_fade_box_state_changed(value):
+        toggle_fadein(value)
+
+    def on_slider_value_changed(self, value):
+        set_volume(value)
+        self.volume_label.setText(f"Volume: {value}%")
+
+# Arguments and stuff
+def main():
+    global args
+    parser = argparse.ArgumentParser(description="Print the username if provided as an argument.")
+    parser.add_argument('-u', '--username', help='Set username on startup')
+    parser.add_argument('-v', '--volume', help='Specify volume on startup (0 - 100)')
+
+    args = parser.parse_args()
+
+    if args.username:
+        print(args.username)
 
 if __name__ == "__main__":
+    main()
     app = QtWidgets.QApplication(sys.argv)
     window = MyMainWindow()
     window.show()
     sys.exit(app.exec())
+
 
 
 # if __name__ == "__main__":
